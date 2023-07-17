40fb7f5ab9b6801dfcf7cf44204144ece6a309f1
Ice Liu <Ice.Liu@vertiv.com>
2023-07-16 21:35:33 -0500

    Remove minimap pdf from Doom Emacs

--- config/doom-emacs/init.el
+++ config/doom-emacs/init.el
@@ -37,8 +37,8 @@
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ligatures           ; ligatures and symbols to make your code pretty again
-       minimap             ; show a map of the code on the side
-       modeline            ; snazzy, Atom-inspired modeline, plus API
+       ;;minimap           ; show a map of the code on the side
+       ;;modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
@@ -99,7 +99,7 @@
        magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf                 ; pdf enhancements
+       ;;pdf                 ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        rgb                 ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
