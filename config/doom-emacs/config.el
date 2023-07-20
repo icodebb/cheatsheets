22681964ca863efbbfaa92f5c611d3df672b7317
Ice Liu <Ice.Liu@vertiv.com>
2023-07-20 15:50:24 -0500

    Org bullets and emacs keymap entry for VS Code

--- config/doom-emacs/config.el
+++ config/doom-emacs/config.el
@@ -78,7 +78,7 @@
 ;; Get pretty org-bullets in Doom Emacs
 (setq
         ;; org-superstar-headline-bullets-list '("⁖" "◉" "○" "✸" "✿")
-        org-superstar-headline-bullets-list '("💀" "🌿" "🥚" "⚓" "🪁")
+        org-superstar-headline-bullets-list '("🧅" "🌿" "🥚" "⚓" "🪁")
 )
 
 ;; Disable the highlight line mode, doesn't work well for Doom
